aaf243cdf73181d9f628a5aa5f74522bb9ef8a85
yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-02-20 21:43:45 -0800

    Virus 3d model in progress.

--- animation/static/js/zdog-demo.js
+++ animation/static/js/zdog-demo.js
@@ -1,216 +1,75 @@
 // zdog-demo.js
+// flag
+let isSpinning = true;
 
 // create illo
 const illo = new Zdog.Illustration({
   // set canvas with selector
   element: '.zdog-canvas',
+  rotate: { x: -Zdog.TAU/16 },
   dragRotate: true,
+    // manipulate the flag when 
+  // the dragging starts and ends
+  onDragStart() {
+    isSpinning = false
+  },
+  onDragEnd() {
+    isSpinning = true
+  }
 });
 
 // Dot for line
 var body = new Zdog.Shape({
   addTo: illo,
-  diameter: 200,
-  stroke: 200,
-  color: "#b94646",
+  diameter: 160,
+  stroke: 160,
+  color: "#696969",
 });
 
-// // origin dot
-// new Zdog.Shape({
-//   addTo: body,
-//   stroke: 8,
-//   color: "#ff0080",
-// });
+// Create mirror zdog.Group
+[false, true].forEach(function (isGroup) {
 
-const circle1 = new Zdog.Ellipse({
-  addTo: illo,
-  diameter: 20,
-  closed: true,
-  translate: { z: 20, x: 0, y: 0 },
-  scale: 1,
-  stroke: 8,
-  fill: true,
-  color: "#ff0000",
-});
-// z line
-new Zdog.Shape({
-  addTo: circle1,
-  path: [ {}, circle1.translate.copy().multiply({ z: -1 }) ],
-  scale: 1/circle1.scale.z,
-  stroke: 4,
-  color: "#ff0000",
-  // backface: false
+  let SliceClass = isGroup ? Zdog.Group : Zdog.Group;
+
+  let dotSlice = new SliceClass({
+    addTo: illo,
+    translate: { z: isGroup ? 74 : -74 },
 });
 
-// copy
-circle1.copyGraph({
-  translate:{ z: 20, x: 40, y: 20},
+// Define the shape position
+let tri = new Zdog.Polygon({
+  addTo: dotSlice,
+  radius: 4,
+  sides: 3,
+  stroke: 10,
+  color: isGroup ? '#ff0000' : '#ff0000',
+});
+// lower right
+tri.copy({ translate: { x:  40, y:  20 } });
+tri.copy({ translate: { x:  60, y:  50 } });
+tri.copy({ translate: { x:  10, y:  50 } });
+// upper right
+tri.copy({ translate: { x:  40, y:  -20 } });
+tri.copy({ translate: { x:  50, y:  -60 } });
+tri.copy({ translate: { x:  5, y:  -50 } });
+// lower left
+tri.copy({ translate: { x:  -20, y:  30 } });
+tri.copy({ translate: { x:  -40, y:  64 } });
+tri.copy({ translate: { x:  -50, y:  15 } });
+// upper left
+tri.copy({ translate: { x:  -60, y:  -50 } });
+tri.copy({ translate: { x:  -40, y:  -20 } });
 });
 
-circle1.copyGraph({
-  translate:{ z: 20, x: 80, y: 0},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: 20, y: 60},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -40, y: 20},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -80, y: 0},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: 20, y: -60},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -40, y: -20},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -80, y: -60},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -20, y: -60},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -40, y: -90},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -20, y: 80},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -60, y: 50},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: -40, y: -20},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: 80, y: 60},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: 80, y: -60},
-});
-
-circle1.copyGraph({
-  translate:{ z: 20, x: 40, y: -30},
-});
-
-// // render shapes in order added
-// var virus = new Zdog.Group({
-//   addTo: illo,
-//   translate: { z: 10 },
-// });  
-
-// // add circle
-// var body = new Zdog.Cylinder({
-//   addTo: virus,
-//   diameter: 100,
-//   stroke: 100,
-//   color: '#b94646',
-// });
-
-// var popCircleAnchor = new Zdog.Anchor({
-//   addTo: body,
-// });
-
-// var popCircle= ( body.diameter + body.stroke )/2 +1 ;
-// // popcircles
-// new Zdog.Ellipse({
-//   addTo: popCircleAnchor,
-//   path: [ { y: -10 }, { y: 10 } ],
-//   translate: { z: popCircle },
-//   diameter: 8,
-//   stroke: 8,
-//   color: "#ff0000",
-// });
-
-// // Dot for line
-// new Zdog.Shape({
-//   addTo: body,
-//   path: [ {}, body.translate.copy().multiply({ z: -1 }) ],
-//   scale: 1/body.scale.z,
-//   stroke: 8,
-//   color: "#ff0080",
-// });
-
-// var circle1 = popCircleAnchor.copyGraph({
-//   rotate: { x: 0.4 },
-// });
-
-// // line
-// new Zdog.Shape({
-//   addTo: circle1,
-//   path: [ {}, circle1.translate.copy().multiply({ z: -2 }) ],
-//   stroke: 8,
-//   color: "#ff0080",
-// });
-
-// var circle2 = popCircleAnchor.copyGraph({
-//   rotate: { x: -0.4 },
-// });
-// var circle3 = popCircleAnchor.copyGraph({
-//   rotate: { x: 0.8 },
-// });
-// var circle4 = popCircleAnchor.copyGraph({
-//   rotate: { x: -0.8 },
-// });
-// var circle5 = popCircleAnchor.copyGraph({
-//   rotate: { y: 0.5 },
-// });
-// var circle6 = popCircleAnchor.copyGraph({
-//   rotate: { y: 0.9 },
-// });
-// var circle7 = popCircleAnchor.copyGraph({
-//   rotate: { y: -0.5 },
-// });
-// var circle8 = popCircleAnchor.copyGraph({
-//   rotate: { y: -0.9 },
-// });
-// var circle9 = popCircleAnchor.copyGraph({
-//   rotate: { x:0.3, y: -0.3 },
-// });
-// var circle10 = popCircleAnchor.copyGraph({
-//   rotate: { x:0.6, y: -0.6 },
-// });
-// var circle11 = popCircleAnchor.copyGraph({
-//   rotate: { x:0.3, y: 0.3 },
-// });
-// var circle12 = popCircleAnchor.copyGraph({
-//   rotate: { x:0.6, y: 0.6 },
-// });
-// var circle13 = popCircleAnchor.copyGraph({
-//   rotate: { x:-0.3, y: 0.3 },
-// });
-// var circle14 = popCircleAnchor.copyGraph({
-//   rotate: { x:-0.6, y: 0.6 },
-// });
-// var circle15 = popCircleAnchor.copyGraph({
-//   rotate: { x:-0.3, y: -0.3 },
-// });
-// var circle16 = popCircleAnchor.copyGraph({
-//   rotate: { x:-0.6, y: -0.6 },
-// });
   
 // update & render
 illo.updateRenderGraph();
   
 function animate() {
   // rotate illo each frame
-  // illo.rotate.y += 0.02;
-  illo.rotate.x += 0.02;
-  illo.rotate.z += 0.02;
+  illo.rotate.y += 0.01;
+  // illo.rotate.x += 0.02;
+  // illo.rotate.z += 0.02;
   illo.updateRenderGraph();
   // animate next frame
   requestAnimationFrame( animate );
