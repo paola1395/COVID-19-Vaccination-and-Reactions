8a42b29f00d2beb3845375f8414dadc24befcef9
leonardz17 <71532759+leonardz17@users.noreply.github.com>
2021-02-20 23:49:27 -0800

    boundaries added for leaflet, working on reading both on same layer

--- Coding/logic.js
+++ Coding/logic.js
@@ -1,8 +1,9 @@
 var link = "dataset.geojson";
+var boundaryLink = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson"
 
 var myMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 5
+    center: [52.2973333,-46.2556329],
+    zoom: 3
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -14,24 +15,62 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-d3.json(link, function(data) {
-    console.log(data.features);
+function chooseColor (x) {
+    var color = "";
+    if (x >= 3000000) {
+        color = "green"
+    }
+    else if (x >= 2500000) {
+        color = "orange"
+    }
+    else if (x >= 2000000){
+        color = "yellow"
+    }
+    else if (x >= 1500000){
+        color = "purple"
+    }
+    else if (x >= 1000000){
+        color = "blue"
+    }
+    else {
+        color = "yellow"
+    }
+    return color;
+}
 
-    L.geoJSON(data, {
-        pointToLayer: function(feature, latlng){
-            return L.circleMarker(latlng, {
-                radius: feature.properties.total_vaccinations/200000,
-                fillColor: "red",
-                color: "white",
-                weight: 1,
-                opacity: 0.6,
-                fillOpacity: 0.6
-            })
-        },
-        onEachFeature: function(feature, layer){
-            layer.bindPopup(feature.properties.location + "<hr> " +feature.properties.total_vaccinations);
-        }
-    }).addTo(myMap);
-
+d3.json(link, function(boundary) {
+    d3.json(boundaryLink, function(data) {
+        L.geoJSON(data, {
+            style: function(feature) {
+                if (feature.properties.location !== feature.properties.location){
+                    return {
+                        color: "blue",
+                        weight: 1.5,
+                        fillOpacity: 0.5,
+                        fillColor: chooseColor(feature.properties.people_fully_vaccinated)
+                    }
+                }    
+            },
+            onEachFeature: function(feature, layer) {
+                layer.on({
+                    mouseover: function(event) {
+                        layer = event.target;
+                        layer.setStyle({
+                            fillOpacity: 0.9
+                        });
+                    },
+                    mouseout: function(event) {
+                        layer = event.target;
+                        layer.setStyle({
+                            fillOpacity: 0.5
+                        });
+                    },
+                    click: function(event) {
+                        myMap.fitBounds(event.target.getBounds());
+                    }
+                });
+                layer.bindPopup(feature.properties.location + "<hr> " + feature.properties.people_fully_vaccinated);
+            },
+        }).addTo(myMap);          
+    });
 });
-
