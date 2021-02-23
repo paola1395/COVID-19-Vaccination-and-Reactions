984e9b47f4f096ec8dfcdb3ab1df591238b81e88
leonardz17 <71532759+leonardz17@users.noreply.github.com>
2021-02-22 22:47:04 -0800

    updated code to add coord to dataset geojson

--- Coding/logic.js
+++ Coding/logic.js
@@ -1,5 +1,5 @@
 var link = "dataset.geojson";
-var boundaryLink = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson"
+var boundaryLink = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson";
 
 var myMap = L.map("map", {
     center: [52.2973333,-46.2556329],
@@ -33,44 +33,116 @@ function chooseColor (x) {
         color = "blue"
     }
     else {
-        color = "yellow"
+        color = "red"
     }
     return color;
 }
+// ISO_A3
 
-d3.json(link, function(boundary) {
-    d3.json(boundaryLink, function(data) {
-        L.geoJSON(data, {
-            style: function(feature) {
-                if (feature.properties.location !== feature.properties.location){
-                    return {
-                        color: "blue",
-                        weight: 1.5,
-                        fillOpacity: 0.5,
-                        fillColor: chooseColor(feature.properties.people_fully_vaccinated)
-                    }
-                }    
-            },
-            onEachFeature: function(feature, layer) {
-                layer.on({
-                    mouseover: function(event) {
-                        layer = event.target;
-                        layer.setStyle({
-                            fillOpacity: 0.9
-                        });
-                    },
-                    mouseout: function(event) {
-                        layer = event.target;
-                        layer.setStyle({
-                            fillOpacity: 0.5
-                        });
-                    },
-                    click: function(event) {
-                        myMap.fitBounds(event.target.getBounds());
-                    }
-                });
-                layer.bindPopup(feature.properties.location + "<hr> " + feature.properties.people_fully_vaccinated);
-            },
-        }).addTo(myMap);          
-    });
-});
+// new_features = {
+//     "features":[
+//         {
+//             "geometry":[],
+//             "properties":
+//         }
+//     ]
+// }
+d3.json(boundaryLink, function(b){
+    var all_coord = b.features;
+    d3.json(link, function(data){
+        data.features.forEach(element => {
+            var extra = all_coord.filter(d=>d.properties.ISO_A3===element.properties.iso_code);
+            console.log(extra.map(d=>d.geometry));
+            element.properties.extra = extra.map(d=>d.geometry);
+            console.log(element.properties);
+            L.geoJSON(element.properties.extra).addTo(myMap);
+            
+                 
+        })
+    })
+})
+
+// d3.json(link, function(data){
+//     // console.log(data.features.filter(d=>d.properties.iso_code==="ARG"))
+//     console.log(data.features);
+//     var all_features = data.features;
+//     d3.json(boundaryLink, function(boundary) {
+//         console.log(boundary);
+//         boundary.features.forEach(element => {
+//             console.log(element.properties.ISO_A3);
+//             var extra = all_features.filter(d=>d.properties.iso_code===element.properties.ISO_A3);
+//             // console.log(extra.map(d=>d.properties));
+//             element.properties.extra = extra.map(d=>d.properties);
+            
+//         });
+//         L.geoJSON(boundary, {
+//             style: function(feature) {
+//                 return {
+//                     color: "blue",
+//                     weight: 1.5,
+//                     fillOpacity: 0.5,
+//                     fillColor: chooseColor()
+//                 }    
+//             },
+//             onEachFeature: function(feature, layer) {
+//                 layer.on({
+//                     mouseover: function(event) {
+//                         layer = event.target;
+//                         layer.setStyle({
+//                             fillOpacity: 0.9
+//                         });
+//                     },
+//                     mouseout: function(event) {
+//                         layer = event.target;
+//                         layer.setStyle({
+//                             fillOpacity: 0.5
+//                         });
+//                     },
+//                     click: function(event) {
+//                         myMap.fitBounds(event.target.getBounds());
+//                     }
+//                 });
+//                 layer.bindPopup(feature.location + "<hr> " + feature.people_fully_vaccinated);
+//             },
+//         }).addTo(myMap);          
+//     });
+    
+// })
+
+
+
+// d3.json(link, function(boundary) {
+
+//     d3.json(boundaryLink, function(data) {
+//         L.geoJSON(data, {
+//             style: function(feature) {
+//                     return {
+//                         color: "blue",
+//                         weight: 1.5,
+//                         fillOpacity: 0.5,
+//                         fillColor: chooseColor(feature.properties.people_fully_vaccinated)
+//                     } 
+//             },
+//             onEachFeature: function(feature, layer) {
+//                 layer.on({
+//                     mouseover: function(event) {
+//                         layer = event.target;
+//                         layer.setStyle({
+//                             fillOpacity: 0.9
+//                         });
+//                     },
+//                     mouseout: function(event) {
+//                         layer = event.target;
+//                         layer.setStyle({
+//                             fillOpacity: 0.5
+//                         });
+//                     },
+//                     click: function(event) {
+//                         myMap.fitBounds(event.target.getBounds());
+//                     }
+//                 });
+//                 layer.bindPopup(feature.properties.location + "<hr> " + feature.properties.people_fully_vaccinated);
+//             },
+//         }).addTo(myMap);          
+//     });
+// });
