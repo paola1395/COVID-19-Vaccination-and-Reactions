d7222da7d001aa64c757aefb897a2560e4a0644f
leonardz17 <71532759+leonardz17@users.noreply.github.com>
2021-02-24 00:22:01 -0800

    test2.js semi working, map is displayed with color function

--- Coding/logic.js
+++ Coding/logic.js
@@ -1,5 +1,5 @@
-var link = "dataset.geojson";
-var boundaryLink = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson";
+const link = "dataset.geojson";
+const boundaryLink = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson";
 
 var myMap = L.map("map", {
     center: [52.2973333,-46.2556329],
@@ -37,112 +37,65 @@ function chooseColor (x) {
     }
     return color;
 }
-// ISO_A3
 
-// new_features = {
-//     "features":[
-//         {
-//             "geometry":[],
-//             "properties":
-//         }
-//     ]
-// }
-d3.json(boundaryLink, function(b){
-    var all_coord = b.features;
-    d3.json(link, function(data){
-        data.features.forEach(element => {
-            var extra = all_coord.filter(d=>d.properties.ISO_A3===element.properties.iso_code);
-            console.log(extra.map(d=>d.geometry));
-            element.properties.extra = extra.map(d=>d.geometry);
-            console.log(element.properties);
-            L.geoJSON(element.properties.extra).addTo(myMap);
+// d3.json(boundaryLink, function(b){
+//     var all_coord = b.features;
+//     d3.json(link, function(data){
+//         data.features.forEach(element => {
+//             var extra = all_coord.filter(d=>d.properties.ISO_A3===element.properties.iso_code);
+//             console.log(extra.map(d=>d.geometry));
+//             element.properties.extra = extra.map(d=>d.geometry);
+//             console.log(element.properties);
+//             L.geoJSON(element.properties.extra).addTo(myMap);
             
                  
-        })
-    })
-})
-
-// d3.json(link, function(data){
-//     // console.log(data.features.filter(d=>d.properties.iso_code==="ARG"))
-//     console.log(data.features);
-//     var all_features = data.features;
-//     d3.json(boundaryLink, function(boundary) {
-//         console.log(boundary);
-//         boundary.features.forEach(element => {
-//             console.log(element.properties.ISO_A3);
-//             var extra = all_features.filter(d=>d.properties.iso_code===element.properties.ISO_A3);
-//             // console.log(extra.map(d=>d.properties));
-//             element.properties.extra = extra.map(d=>d.properties);
-            
-//         });
-//         L.geoJSON(boundary, {
-//             style: function(feature) {
-//                 return {
-//                     color: "blue",
-//                     weight: 1.5,
-//                     fillOpacity: 0.5,
-//                     fillColor: chooseColor()
-//                 }    
-//             },
-//             onEachFeature: function(feature, layer) {
-//                 layer.on({
-//                     mouseover: function(event) {
-//                         layer = event.target;
-//                         layer.setStyle({
-//                             fillOpacity: 0.9
-//                         });
-//                     },
-//                     mouseout: function(event) {
-//                         layer = event.target;
-//                         layer.setStyle({
-//                             fillOpacity: 0.5
-//                         });
-//                     },
-//                     click: function(event) {
-//                         myMap.fitBounds(event.target.getBounds());
-//                     }
-//                 });
-//                 layer.bindPopup(feature.location + "<hr> " + feature.people_fully_vaccinated);
-//             },
-//         }).addTo(myMap);          
-//     });
-    
+//         })
+//     })
 // })
 
+d3.json(link, function(data){
+    // console.log(data.features);
+    var all_features = data.features;
+    d3.json(boundaryLink, function(boundary) {
+        // console.log(boundary);
+        boundary.features.forEach(element => {
+            var extra = all_features.filter(d=>d.properties.iso_code===element.properties.ISO_A3);
+            element.properties.extra = extra.map(d=>d.properties);
+            coords = extra.map(d=>d.geometry);
+            console.log(coords);
+            // console.log(element.properties);    
+            // L.geoJSON(bounary, {
+            //     style: function(feature) {
+            //         return {
+            //             color: "blue",
+            //             weight: 1.5,
+            //             fillOpacity: 0.5,
+            //             fillColor: chooseColor(element.properties.people_fully_vaccinated)
+            //         } 
+            //     },
+            //     onEachFeature: function(feature, layer) {
+            //         layer.on({
+            //             mouseover: function(event) {
+            //                 layer = event.target;
+            //                 layer.setStyle({
+            //                     fillOpacity: 0.9
+            //                 });
+            //             },
+            //             mouseout: function(event) {
+            //                 layer = event.target;
+            //                 layer.setStyle({
+            //                     fillOpacity: 0.5
+            //                 });
+            //             },
+            //             click: function(event) {
+            //                 myMap.fitBounds(event.target.getBounds());
+            //             }
+            //         });
+            //         layer.bindPopup(element.properties.location + "<hr> " + element.properties.people_fully_vaccinated);
+            //     },
+            // }).addTo(myMap);
+        })          
+    });
+    
+})
 
-
-// d3.json(link, function(boundary) {
-
-//     d3.json(boundaryLink, function(data) {
-//         L.geoJSON(data, {
-//             style: function(feature) {
-//                     return {
-//                         color: "blue",
-//                         weight: 1.5,
-//                         fillOpacity: 0.5,
-//                         fillColor: chooseColor(feature.properties.people_fully_vaccinated)
-//                     } 
-//             },
-//             onEachFeature: function(feature, layer) {
-//                 layer.on({
-//                     mouseover: function(event) {
-//                         layer = event.target;
-//                         layer.setStyle({
-//                             fillOpacity: 0.9
-//                         });
-//                     },
-//                     mouseout: function(event) {
-//                         layer = event.target;
-//                         layer.setStyle({
-//                             fillOpacity: 0.5
-//                         });
-//                     },
-//                     click: function(event) {
-//                         myMap.fitBounds(event.target.getBounds());
-//                     }
-//                 });
-//                 layer.bindPopup(feature.properties.location + "<hr> " + feature.properties.people_fully_vaccinated);
-//             },
-//         }).addTo(myMap);          
-//     });
-// });
